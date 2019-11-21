27f84eec0ebba8e61b7efee2c55cb063f64e1af9
rishil77 <57910960+rishil77@users.noreply.github.com>
2019-11-21 11:31:18 -0500

    Update teamr.py

--- teamr.py
+++ teamr.py
@@ -1,46 +1,48 @@
 from turtle import *
-R = Turtle()
+import math
+from random import *
+r= Turtle()
 colormode(255)
+tracer(0)
 
 def createRipple():
-    #By Rishil Patel
-    R.setheading(270) #face south
+    #By rishil Patel
+    r.setheading(270) #face south
     for i in range(180): #create a half circle
-        R.forward(1)
-        R.right(1)
+        r.forward(1)
+        r.right(1)
 def createWaves():
-    #By Rishil Patel
-    R.pencolor("blue")
-    R.fillcolor("blue")
-    R.begin_fill()
-    R.penup()
-    R.goto(475,200) #top right corner
-    R.pendown()
+    #By rishil Patel
+    r.pencolor("blue")
+    r.fillcolor("blue")
+    r.begin_fill()
+    r.penup()
+    r.goto(475,200) #top right corner
+    r.pendown()
     for i in range(4): #create 4 ripples
         createRipple()
-    R.setheading(270)
-    R.forward(700) #go to bottom left corner
-    R.left(90)
-    R.forward(1200) #go to bottom right corner
-    R.goto(475,200) #go to top right corner
-    R.end_fill()
+    r.setheading(270)
+    r.forward(700) #go to bottom left corner
+    r.left(90)
+    r.forward(1200) #go to bottom right corner
+    r.goto(475,200) #go to top right corner
+    r.end_fill()
 
 def drawLand():
-    #By Rishil Patel
-    R.penup()
-    R.pencolor("light green") #color of the land
-    R.begin_fill()
-    R.fillcolor("light green")
-    R.goto(-500,200) 
-    R.pendown()
-    R.right(90)
-    R.forward(800) 
-    R.left(90)
-    R.forward(((360/math.pi)*4)+60) #goes to the wave's edge
-    R.left(90)
-    R.forward(800)
-    R.left(90)
-    R.forward(((360/math.pi)*4)+60) #goes to the wave's edge
-    R.end_fill() #fills half the page with the land
-
+    #By rishil Patel
+    r.penup()
+    r.pencolor("light green") #color of the land
+    r.begin_fill()
+    r.fillcolor("light green")
+    r.goto(-500,200) 
+    r.pendown()
+    r.right(90)
+    r.forward(800) 
+    r.left(90)
+    r.forward(((360/math.pi)*4)+60) #goes to the wave's edge
+    r.left(90)
+    r.forward(800)
+    r.left(90)
+    r.forward(((360/math.pi)*4)+60) #goes to the wave's edge
+    r.end_fill() #fills half the page with the land
 update()
