06293f52465135b47d14423c2277689e86ba5afb
rishil77 <57910960+rishil77@users.noreply.github.com>
2019-11-21 11:26:58 -0500

    Update teamr.py

--- teamr.py
+++ teamr.py
@@ -2,16 +2,44 @@ from turtle import *
 r = Turtle()
 colormode(255)
 
-def reminder():
-  # by Mr. Riley
-  # don't forget to put a comment with your name at the top of each function you make
-  # this will help to keep track of who created each function
-  return True
-  
-def makeCresentMoon():
-  lksjfdsdf
-  kjsldfkjsdf
-  return
+def createRipple():
+    #By Rishil Patel
+    R.setheading(270) #face south
+    for i in range(180): #create a half circle
+        R.forward(1)
+        R.right(1)
+def createWaves():
+    #By Rishil Patel
+    R.pencolor("blue")
+    R.fillcolor("blue")
+    R.begin_fill()
+    R.penup()
+    R.goto(475,200) #top right corner
+    R.pendown()
+    for i in range(4): #create 4 ripples
+        createRipple()
+    R.setheading(270)
+    R.forward(700) #go to bottom left corner
+    R.left(90)
+    R.forward(1200) #go to bottom right corner
+    R.goto(475,200) #go to top right corner
+    R.end_fill()
 
+def drawLand():
+    R.penup()
+    R.pencolor("light green") #color of the land
+    R.begin_fill()
+    R.fillcolor("light green")
+    R.goto(-500,200) 
+    R.pendown()
+    R.right(90)
+    R.forward(800) 
+    R.left(90)
+    R.forward(((360/math.pi)*4)+60) #goes to the wave's edge
+    R.left(90)
+    R.forward(800)
+    R.left(90)
+    R.forward(((360/math.pi)*4)+60) #goes to the wave's edge
+    R.end_fill() #fills half the page with the land
 
 update()
